7c21d7aea799e30d4113de5086b24af9fdca6053
AdamXMira <mira1@kenyon.edu>
2024-02-06 22:20:15 -0500

    modified:   SearchPrefix.rb

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -2,7 +2,8 @@ def searchPrefix(arr,s)
     found = arr.select {|i| i.start_with?(s)}
     return found
 end
-
+puts "the strings in the array starting with 'ab' are: "
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
-
+puts
+puts "the strings in the array starting with 'b' are: "
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
